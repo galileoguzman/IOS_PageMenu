93931ca268fac346ba5c6e75b75018463d899b2a
Galileo Guzmán <galileoguzman@gmail.com>
2016-01-27 13:58:31 -0600

    Initial Commit

---
+++ Paging/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Paging
+//
+//  Created by Galileo Guzman on 27/01/16.
+//  Copyright © 2016 Galileo Guzman. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
